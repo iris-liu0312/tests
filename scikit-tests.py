bf2fa9e6afed5133e4581ddd8e1bd7c99efa4ace
iris-liu0312 <iris.liu0312@gmail.com>
2021-06-21 14:33:20 -0600

    second commit

---
+++ scikit-tests.py
@@ -0,0 +1,35 @@
+import skvideo.io
+import skvideo.measure
+import time
+import numpy as np
+
+
+times = [time.time()]
+print("loading...", end='')
+inputdata = skvideo.io.vread("../../Desktop/Videos/8_min_480.mp4", num_frames=120, outputdict={"-pix_fmt": "gray"})[:, :, :, 0]
+print("finished.")
+times.append(time.time())
+
+# print("searching...", end='')
+# key_frames = skvideo.measure.scenedet(inputdata)
+# print(f"{len(key_frames)} key frames found.")
+# times.append(time.time())
+# inputdata = inputdata[key_frames, :, :]
+
+# test score
+print("calculating viideo...", end='')
+viideo = skvideo.measure.viideo_score(inputdata)
+print("finished.")
+times.append(time.time())
+
+print("calculating niqe...", end='')
+niqe = np.mean(skvideo.measure.niqe(inputdata))
+print("finished.")
+times.append(time.time())
+
+print(f"================\n"
+      f"Total time: {time.time() - times[0]}s\n"
+      f"NIQE: {niqe}, {times[-1] - times[-2]}s\n"
+      f"VIIDEO: {viideo}, {times[-2] - times[-3]}s\n"
+      f"frames: {inputdata.shape[0]}\n"
+      f"================")
