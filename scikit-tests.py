74ae15f2bac0f5b3cff1a8a6fb5aeb5c610e1e10
iris-liu0312 <iris.liu0312@gmail.com>
2021-06-22 10:13:54 -0600

    better prints for cli

--- scikit-tests.py
+++ scikit-tests.py
@@ -5,9 +5,8 @@ import numpy as np
 
 
 times = [time.time()]
-print("loading...", end='')
 inputdata = skvideo.io.vread("./videos/8_min_480.mp4", num_frames=15, outputdict={"-pix_fmt": "gray"})[:, :, :, 0]
-print("finished.")
+print("loading finished. calculating viideo.")
 times.append(time.time())
 
 # print("searching...", end='')
@@ -17,19 +16,17 @@ times.append(time.time())
 # inputdata = inputdata[key_frames, :, :]
 
 # test score
-print("calculating viideo...", end='')
 viideo = skvideo.measure.viideo_score(inputdata)
-print("finished.")
+print("viideo finished. calculating niqe.")
 times.append(time.time())
 
-print("calculating niqe...", end='')
 niqe = np.mean(skvideo.measure.niqe(inputdata))
-print("finished.")
+print("niqe finished.")
 times.append(time.time())
 
-print(f"================\n"
+print(f"=============================================\n"
       f"Total time: {time.time() - times[0]}s\n"
       f"NIQE: {niqe}, {times[-1] - times[-2]}s\n"
       f"VIIDEO: {viideo}, {times[-2] - times[-3]}s\n"
       f"frames: {inputdata.shape[0]}\n"
-      f"================")
+      f"=============================================")
