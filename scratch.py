e580acadb36a596185af5244d63e4873f7d2ee34
iris-liu0312 <iris.liu0312@gmail.com>
2021-07-06 12:40:09 -0600

    fitted cov now (36,36) = default cov

--- scratch.py
+++ scratch.py
@@ -4,3 +4,5 @@ import scipy.io as sio
 estimateniqe.estimate_model_param('pristine')
 data = sio.loadmat('niqe_image_params.mat')
 print(f"cov shape: {data['pop_cov'].shape}")
+data = sio.loadmat('niqe_fitted_parameters.mat')
+print(f"fitted cov shape: {data['pop_cov'].shape}")
