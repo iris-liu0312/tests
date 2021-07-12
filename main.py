31a28098d0ece266452da25ac763c7aa4b7bcd7f
iris-liu0312 <iris.liu0312@gmail.com>
2021-07-12 09:47:52 -0600

    NIQE only

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python
 import getopt
 import sys
+import os
 
 from scores import *
 
 
 def main(argv):
     h = """main.py
-    Input the video you want to analyze as well as some other options.
-    If no directory for training is specified, the default NIQE is used.
+    Input the video you want to calculate NIQE for.
+    If no directory for training or mat file is specified,
+    the default NIQE is used.
     -h      help
     -i      input video file to be analyzed 
     
     Optionals:
     -f      frames to analyze, default 150
-    -t [n] specifies the metrics to calculate. default n
-            v = VIIDEO
-            n = NIQE
     -d      directory for image database to train NIQE
             or parameters mat file for NIQE
     """
     input_file = ''
     frames = 150
-    t = 'n'
     path = ''
     try:
-        opts, args = getopt.getopt(argv, "hi:f:d:m:t:", ["help", "inputfile=", "frames=",
-                                                         "directory=", "tests="])
+        opts, args = getopt.getopt(argv, "hi:f:d:m:", ["help", "inputfile=", "frames=",
+                                                       "directory="])
     except getopt.GetoptError:
         print(h)
         sys.exit(2)
@@ -40,20 +38,16 @@ def main(argv):
             frames = arg
         elif opt in ("-d", "--directory"):
             path = arg
-        elif opt in ("-t", "--tests"):
-            t = arg
-    print(f'input file, {input_file}\n',
+    print(f'input file, {os.path.basename(os.path.normpath(input_file))}\n',
           f'frames, {frames}\n',
-          f'tests, {t}')
-    if not path == '':
-        print(f'path, {path}')
+          f'path, {os.path.basename(os.path.normpath(path))}')
     if input_file == '':
         print(h)
         sys.exit(2)
     if path:
-        fit_test(input_file, frames, path, t)
+        fit_test(input_file, frames, path)
     else:
-        test(input_file, frames, t)
+        test(input_file, frames)
 
 
 if __name__ == "__main__":
--- scores.py
+++ scores.py
@@ -7,30 +7,20 @@ import skvideo.measure
 import estimateniqe
 
 
-def test(video, frames, t):
-    viideo = -1
-    niqe = -1
+def test(video, frames):
     times = [time.time()]
     inputdata = skvideo.io.vread(video, num_frames=frames, outputdict={"-pix_fmt": "gray"})[:, :, :, 0]
     times.append(time.time())
 
     # test score
-    if "v" in t:
-        viideo = skvideo.measure.viideo_score(inputdata)
-        times.append(time.time())
+    niqe = np.mean(skvideo.measure.niqe(inputdata))
+    times.append(time.time())
 
-    if "n" in t:
-        niqe = np.mean(skvideo.measure.niqe(inputdata))
-        times.append(time.time())
-
-    print(f"NIQE, {round(niqe, 5)}, time, {round(times[-1] - times[-2])}\n" * ("n" in t),
-          f"VIIDEO, {round(viideo, 5)}, time, {round(times[-2] - times[-3])}\n" * ("v" in t),
-          f"Total time,  {round(time.time() - times[0])}\n" * ("n" in t) * ("v" in t))
+    print(f"NIQE, {round(niqe, 5)}\n"
+          f"time, {round(times[-1] - times[-2])}")
 
 
-def fit_test(video, frames, path, t):
-    viideo = -1
-    niqe = -1
+def fit_test(video, frames, path):
 
     model = path
     if os.path.isdir(path):
@@ -42,14 +32,8 @@ def fit_test(video, frames, path, t):
     times.append(time.time())
 
     # test score
-    if t in ("v", "V"):
-        viideo = skvideo.measure.viideo_score(inputdata)
-        times.append(time.time())
+    niqe = np.mean(estimateniqe.fit_niqe(inputdata, model))
+    times.append(time.time())
 
-    if t in ("n", "N"):
-        niqe = np.mean(estimateniqe.fit_niqe(inputdata, model))
-        times.append(time.time())
-
-    print(f"NIQE, {round(niqe, 5)}, time, {round(times[-1] - times[-2])}\n" * ("n" in t),
-          f"VIIDEO, {round(viideo, 5)}, time, {round(times[-2] - times[-3])}\n" * ("v" in t),
-          f"Total time,  {round(time.time() - times[0])}\n" * ("n" in t) * ("v" in t))
+    print(f"NIQE, {round(niqe, 5)}\n",
+          f"time, {round(times[-1] - times[-2])}")
