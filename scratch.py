58ca496da2110a027467204c56b1f0455636134b
iris-liu0312 <iris.liu0312@gmail.com>
2021-07-12 09:48:27 -0600

    removed scratch paper

--- scratch.py
+++
@@ -1,8 +0,0 @@
-import estimateniqe
-import scipy.io as sio
-
-estimateniqe.estimate_model_param('pristine')
-data = sio.loadmat('niqe_image_params.mat')
-print(f"cov shape: {data['pop_cov'].shape}")
-data = sio.loadmat('niqe_fitted_parameters.mat')
-print(f"fitted cov shape: {data['pop_cov'].shape}")
