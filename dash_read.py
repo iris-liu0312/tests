29160efcfcc70ec21d1710e84f1028f2412de803
iris-liu0312 <iris.liu0312@gmail.com>
2021-07-14 09:38:35 -0600

    first commit

---
+++ dash_read.py
@@ -0,0 +1,61 @@
+#!/bin/python
+import getopt
+import os
+import sys
+import urllib.request
+
+import main
+
+
+def dash_read(argv):
+    h = """hls_read.py
+    Reads HLS manifest and goes into the highest quality playlist.
+    Obtains last video segment available on the playlist.
+    The video segment is saved to the current destination, and NIQE is calculated.
+    -h    help
+    -u    url
+    """
+    # obtain arguments
+    url = ''
+    try:
+        opts, args = getopt.getopt(argv, "hu:", ["help", "url="])
+    except getopt.GetoptError:
+        print(h)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(h)
+            sys.exit()
+        if opt in ("-u", "--url"):
+            url = arg
+
+    # exit with help
+    if url == '':
+        print(h)
+        sys.exit(2)
+
+    # extract home-url
+    # home = url.replace(os.path.basename(url), '')
+    # manifest = urllib.request.urlopen(url)
+    # found = False
+    # for line in manifest:
+    #     decoded_line = line.decode("utf-8")
+    #     if found:
+    #         manifest = home+decoded_line
+    #         break
+    #     if "STREAM-INF" in decoded_line:
+    #         found = True
+    # HTTPResponse object not reversible, will take some time to run through the whole file
+    # playlist = urllib.request.urlopen(manifest)
+    # decoded_line = ''
+    # for line in playlist:
+    #     decoded_line = line.decode("utf-8")
+    #     pass
+    # ts = home+decoded_line.rstrip()
+    # destination = (os.getcwd()+'/'+decoded_line).replace("\r\n", '')
+    # urllib.request.urlretrieve(ts, destination)
+    # main.main(["-i", destination])
+
+
+if __name__ == "__main__":
+    dash_read(sys.argv[1:])
